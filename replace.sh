eb8d4974f4dbf3b73b9a3b5f2565755a3e69b0f8
marysalsita <msalcedo.inf@gmail.com>
2015-03-10 18:48:31 -0430

    Se agrego el yaml para verificar que etcd en cada uno de los host se encuentra activo

--- replace.sh
+++ replace.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-sed -e "s/COUCHBASE-IP/${CB_HOST}/g" /couchbase.yaml > /etc/dd-agent/conf.d/couchbase.yaml
+
+hosts=$ETCD
+instance=''
+
+sed -e "s/COUCHBASE-IP/${CB_HOST}/g" couchbase.yaml > /etc/dd-agent/conf.d/couchbase.yaml
+
+for x in $hosts
+do
+    instance=$instance'\t- url: "https://'$x'"\n'
+done
+
+echo $instance > help.txt
+
+sed '/instances:/r help.txt' etcd.yaml > /etc/dd-agent/conf.d/etcd.yaml
 
 /entrypoint.sh "$@"
