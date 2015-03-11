9e342faefaf71644add4052896dd243e94ae106d
marysalsita <msalcedo.inf@gmail.com>
2015-03-11 13:42:03 -0430

    se modifico el archivo donde se escriben los url

--- replace.sh
+++ replace.sh
@@ -13,8 +13,8 @@ done
 
 echo $instance > /etc/dd-agent/conf.d/help.txt
 
-sed 's/^/    /' /etc/dd-agent/conf.d/help.txt > /etc/dd-agent/conf.d/help.txt
+sed 's/^/    /' /etc/dd-agent/conf.d/help.txt > /etc/dd-agent/conf.d/aux.txt
 
-sed '/instances:/r /etc/dd-agent/conf.d/help.txt' etcd.yaml > /etc/dd-agent/conf.d/etcd.yaml
+sed '/instances:/r /etc/dd-agent/conf.d/aux.txt' etcd.yaml > /etc/dd-agent/conf.d/etcd.yaml
 
 /entrypoint.sh "$@"
